e8bc24d3745291544b433a9587a13f130cb0bb9b
KinozHao <kinozhao0715@gmail.com>
2022-08-01 23:13:09 +0800

    SpringMVC

--- B_RequestMapping/src/main/java/com/Index.java
+++ B_RequestMapping/src/main/java/com/Index.java
@@ -1,7 +1,10 @@
 package com;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * @author kinoz
@@ -14,5 +17,32 @@ public class Index {
     @RequestMapping("/")
     public String index(){
         return "index";
+    }
+
+
+    //测试value 数组形式访问多个路径
+    @RequestMapping(value = {"/test1","/test2"})
+    public String index1(){
+        return "valueTest";
+    }
+
+    //测试method 指定请求方式
+    @RequestMapping(value = "/methods",method = {RequestMethod.GET})
+    public String index2(){
+        return "valueTest";
+    }
+    //RequestMapping的派生注解
+    @GetMapping("/rm")
+    public String index3(){
+        return "valueTest";
+    }
+    @PostMapping("/pm")
+    public String index4(){
+        return "valueTest";
+    }
+    //测试params 携带参数
+    @RequestMapping(value = "/paramsTest",params = {"username","password!=123456"})
+    public String index5(){
+        return "valueTest";
     }
 }

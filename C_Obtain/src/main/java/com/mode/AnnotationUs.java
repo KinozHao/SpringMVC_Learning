8b05a7884d72a77d4dd6348110645983bb5ba76f
KinozHao <kinozhao0715@gmail.com>
2022-08-04 20:07:43 +0800

    SpringMVC

--- C_Obtain/src/main/java/com/mode/AnnotationUs.java
+++ C_Obtain/src/main/java/com/mode/AnnotationUs.java
@@ -1,6 +1,8 @@
 package com.mode;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -16,9 +18,15 @@ public class AnnotationUs {
             //required代表此参数是否必填,默认为true
             //defaultValue代表默认值,若没传参情况下默认获取指定默认值
             @RequestParam(value = "user_name",defaultValue = "默认名称",required = false) String username,
-            @RequestParam(value = "secret",defaultValue = "默认密码",required = true) String password){
-        System.out.println(username);
-        System.out.println(password);
+            @RequestParam(value = "secret",required = true) String password,
+            @RequestHeader("Host") String host,
+            @RequestHeader("User-Agent") String agent,
+            @CookieValue("JSESSIONID") String JSESSIONID){
+        //获取到html参数,控制台输出
+        System.out.println(username+" "+password);
+        System.out.println(host);
+        System.out.println(agent);
+        System.out.println("cookie参数:"+JSESSIONID);
         return "success";
     }
 }
--- C_Obtain/src/main/java/com/mode/ServletUs.java
+++ C_Obtain/src/main/java/com/mode/ServletUs.java
@@ -1,14 +1,20 @@
 package com.mode;
 
+import com.entity.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.ServletRequest;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 /**
  * @author kinoz
  * @Date 2022/8/3 - 15:32
  * @apiNote 使用servlet api实现参数传递
+ * @see AnnotationUs 测试cookieValue
  */
 @Controller
 public class ServletUs {
@@ -19,10 +25,24 @@ public class ServletUs {
     }
 
     @RequestMapping(value = "/testServletAPI",params = {"username","password"})
-    public String index1(ServletRequest req){
+    public String index1(HttpServletRequest req){
+        //测试cookieValue注解
+        HttpSession session = req.getSession();
         //通过原生servlet方法获取前端参数
         System.out.println(req.getParameter("username"));
         System.out.println(req.getParameter("password"));
+        System.out.println(session);
+        return "success";
+    }
+
+    //实体类参数获取测试
+    @RequestMapping("/testPOJO")
+    public String index2(){
+        return "pojo";
+    }
+    @RequestMapping("/POJO")
+    public String index2(User user){
+        System.out.println(user);
         return "success";
     }
 }

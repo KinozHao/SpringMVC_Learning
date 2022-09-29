1bfcd58019e9e4b4f0f37197ad0d85ebaaaf82bf
KinozHao <kinozhao0715@gmail.com>
2022-09-29 19:22:12 +0800

    SpringMVC

--- F_RestProject/src/main/java/top/controller/EmployeeCon.java
+++ F_RestProject/src/main/java/top/controller/EmployeeCon.java
@@ -1,8 +1,12 @@
 package top.controller;
 
+import org.springframework.beans.factory.BeanClassLoaderAware;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import top.dao.EmployeeDao;
@@ -22,11 +26,43 @@ public class EmployeeCon {
     @Resource
     private EmployeeDao employeeDao;
 
+
     //获取全部用户信息
-    @RequestMapping(value = "employee", method = RequestMethod.GET)
+    @RequestMapping(value = "/employee", method = RequestMethod.GET)
     public String getEmployeeList(Model model){
         Collection<Employee> employeeList = employeeDao.getAll();
         model.addAttribute("employeeList",employeeList);
         return "employee_list";
+    }
+
+    //删除用户信息
+    @RequestMapping(value = "/employee/{id}",method = RequestMethod.DELETE)
+    public String deleteEmployee(@PathVariable("id") Integer id){
+        employeeDao.delete(id);
+        return "redirect:/employee";    //重定向
+    }
+
+    //添加用户信息
+    @RequestMapping(value = "/employee",method = RequestMethod.POST)
+    public String addEmployee(Employee employee){
+        employeeDao.save(employee);
+        return "redirect:/employee";
+    }
+
+    //更新用户信息
+    // TODO: 2022/9/29 coding have nothing problem
+    //  but when click submit button will notice 405 method 'PUT' not supported
+    //  see another author same code test no problem
+    @RequestMapping(value = "/employee/{id}", method = RequestMethod.GET)
+    public String getEmployeeById(@PathVariable("id") Integer id, Model model){
+        Employee employee = employeeDao.get(id);
+        model.addAttribute("employee", employee);
+        return "employee_update";
+    }
+
+    @RequestMapping(value = "/employee",method = RequestMethod.PUT)
+    public String updateEmployee(Employee employee){
+        employeeDao.save(employee);
+        return "redirect:/employee";
     }
 }
--- F_RestProject/src/main/java/top/dao/EmployeeDao.java
+++ F_RestProject/src/main/java/top/dao/EmployeeDao.java
@@ -1,22 +1,31 @@
 package top.dao;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 import top.entity.Employee;
 
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
  * @author kinoz
  * @Date 2022/9/24 14:53
- * @apiNote  模拟用户数据
+ * @apiNote 模拟用户数据
  */
 @Repository
 public class EmployeeDao {
 
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
     private static Map<Integer, Employee> employees = null;
 
+    //静态数据
     static {
         employees = new HashMap<Integer, Employee>();
         employees.put(1001, new Employee(1001, "E-AA", "aa@163.com", 1));
@@ -36,15 +45,23 @@ public class EmployeeDao {
         if (employee.getId() == null) {
             employee.setId(initId++);
         }
-        employees.put(employee.getId(), employee);
+        //employees.put(employee.getId(), employee);
+        String sql = "insert into restful values(?,?,?,?)";
+        int update = jdbcTemplate.update(sql, employee.getId(), employee.getLastName(), employee.getEmail(), employee.getGender());
+        System.out.println("添加成功:影响行数:"+update);
+
     }
 
     /**
      * 获取所有员工
      * @return
      */
+    @Test
     public Collection<Employee> getAll() {
-        return employees.values();
+        String sql = "select * from restful";
+        List<Employee> employeeList = jdbcTemplate.query(sql, new BeanPropertyRowMapper<>(Employee.class));
+        //return employees.values();
+        return employeeList;
     }
 
     /**
@@ -53,7 +70,10 @@ public class EmployeeDao {
      * @return
      */
     public Employee get(Integer id) {
-        return employees.get(id);
+        String sql = "select * from restful where id = ?";
+        Employee employee = jdbcTemplate.queryForObject(sql, new BeanPropertyRowMapper<>(Employee.class), id);
+        return employee;
+        //return employees.get(id);
     }
 
     /**
@@ -61,6 +81,8 @@ public class EmployeeDao {
      * @param id
      */
     public void delete(Integer id) {
-        employees.remove(id);
+        String sql = "delete from restful where id=?";
+        jdbcTemplate.update(sql,id);
+        //employees.remove(id);
     }
 }
